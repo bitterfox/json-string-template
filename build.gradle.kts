8c88ff428aec3c2e9d2971686f22219efddb610e
yoshida.shinya <yoshida.shinya@linecorp.com>
2024-03-21 20:19:09 +0900

    main Bump 0.21.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ allprojects {
 
     group = "io.github.bitterfox"
     // 0.<java version>.<minor version>
-    version = "0.21.2-SNAPSHOT"
+    version = "0.21.2"
 
     dependencies {
         testImplementation(platform("org.junit:junit-bom:5.9.1"))
