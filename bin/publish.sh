a64f4602582596d6e204c26f1addf594a754418b
yoshida.shinya <yoshida.shinya@linecorp.com>
2024-03-23 00:12:07 +0900

    main Add auto bump version to publish.sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -eo pipefail
+
+sed -i -r "s/(.*version.*)-SNAPSHOT(.*)/\1\2/" ./build.gradle.kts
+git add ./build.gradle.kts
+git commit "main Bump `grep "version = " build.gradle.kts | sed -r "s/.*version = \"(.*)\"/\1/"`"
+
+./gradlew clean build test
+
 ./gradlew :json-string-template-core:publishToCentralPortal
 ./gradlew :json-string-template-jakarta-json:publishToCentralPortal
 ./gradlew :json-string-template-org-json:publishToCentralPortal
